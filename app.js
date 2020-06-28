b79bd313567e8e7777318cf486b9381e49c983f3
Tran Tan Tai <Kuerl14@gmail.com>
2020-06-28 08:43:17 +0700

    Update Cái Sườn

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const PORT = process.env.PORT || 5000;
+var serverr = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+var server = require('socket.io').listen(serverr);
+
+var path = require('path');
+
+
+let client = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//connect html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/app.html'));
+});
+
+server.on('connection', (client) => {
+    console.log("new client connected");
+
+    //take client username
+    client.on('create username', (username) => {
+
+        //take message
+        client.on('chat message', (msg) => {
+            var usermsg = {
+                "username": username,
+                "message": msg
+            };
+            //server.emit('chat message', username + ':' + msg);
+            var data = JSON.stringify(usermsg);
+            fs.writeFile('public/log.json', data, { flag: 'a+' }, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log("JSON data is saved.");
+    
+            });
+        });
+    });
+});
+    module.exports = app;
