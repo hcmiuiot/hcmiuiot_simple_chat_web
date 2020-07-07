8ce02462396aee3cba12195c91aa06f06b1c91ff
Anh Viet Vo <anhviet5121pc@gmail.com>
2020-07-07 19:10:28 +0700

    final test version

--- app.js
+++ app.js
@@ -8,41 +8,36 @@ var server = require('socket.io').listen(serverr);
 
 var path = require('path');
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 //connect html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/main.html'));
+	res.sendFile(path.join(__dirname, 'public/main.html'));
 });
-
-
 
 server.on('connection', (client) => {
-    //console.log("new client connected");
+	//console.log("new client connected");
 
-    //take client username
-    //client.on('create username', (username) => {
+	// take client username
+	// client.on('create username', (username) => {
 
-        //take message
-    client.on('chat message', (msg) => {
-            var usermsg = {
-                //"username": username,
-                "message": msg
-         };
-         console.log('receive message');
-        server.emit('chat message', msg);
-            var data = JSON.stringify(usermsg);
-            fs.writeFile('public/log.json', data, { flag: 'a+' }, (err) => {
-                if (err) {
-                    throw err;
-                }
-        
-            });
-        });
-    //});
+	//take message
+	client.on('chat message', (username, msg) => {
+		var usermsg = {
+			username: username,
+			message: msg,
+		};
+		console.log('receive message');
+		server.emit('chat message', username, msg);
+		var data = JSON.stringify(usermsg);
+		fs.writeFile('public/log.json', data, { flag: 'a+' }, (err) => {
+			if (err) {
+				throw err;
+			}
+		});
+	});
+	//});
 });
-    module.exports = app;
+module.exports = app;
