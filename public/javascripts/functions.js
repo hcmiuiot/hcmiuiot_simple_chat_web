ebd2f7e541cedf0d66e6da9676eecb451635677f
Datdeptrai0609 <Datdeptrai0609>
2020-07-09 05:05:37 +0700

    add commer

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -3,12 +3,12 @@
         var textInput = document.getElementById('txt-input').value;
         var splitText = textInput.split(' ');
         
-            for(let i=0; i<=splitText.length-1; i++) {
-                var result =splitText[i].split('').find(char => char == '.');
-                console.log(result);
-                if( result == '.') {
-                    var append = `<iframe src= ${splitText[i]}> </iframe>`;
+        for(let i=0; i<=splitText.length-1; i++) {
+            var result =splitText[i].split('').find(char => char == '.');
+            if( result == '.') {
+                    var append = `<iframe src=' ${splitText[i]}'> </iframe>`;
                     console.log(append);
+                    try[try.length-1].innerHTML(append);
                     break;
                 };
             }
