3418a152faf0178fdb76dd6ef21a689ea00e57c2
Datdeptrai0609 <Datdeptrai0609>
2020-07-09 01:31:05 +0700

    iframe function

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,5 +1,16 @@
-var form = document.getElementById('submit');
+    var form = document.getElementById('submit');
     form.onclick = () => {
-    var textInput = document.getElementById('txt-input').value;
-    console.log(textInput);
-}
+        var textInput = document.getElementById('txt-input').value;
+        var splitText = textInput.split(' ');
+        
+            for(let i=0; i<=splitText.length-1; i++) {
+                var result =splitText[i].split('').find(char => char == '.');
+                console.log(result);
+                if( result == '.') {
+                    var append = `<iframe src= ${splitText[i]}> </iframe>`;
+                    console.log(append);
+                    break;
+                };
+            }
+        console.log(textInput);
+    }
