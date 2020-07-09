096c5e4166da430b89e9706e69a77c37a6527206
Datdeptrai0609 <Datdeptrai0609>
2020-07-09 09:05:49 +0700

    function iframe done 1/2

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,23 +1,20 @@
     var form = document.getElementById('submit');
+    var append = '';
     form.onclick = () => {
         var textInput = document.getElementById('txt-input').value;
         var splitText = textInput.split(' ');
-        
-<<<<<<< HEAD
-            for(let i=0; i<=splitText.length-1; i++) {
-                var result =splitText[i].split('').find(char => char == '.');
-                console.log(result);
-                if( result == '.') {
-                    var append = `<iframe src="${splitText[i]}"></iframe>`;
-=======
+        var ifr = document.getElementsByClassName('ifr');
+
         for(let i=0; i<=splitText.length-1; i++) {
             var result =splitText[i].split('').find(char => char == '.');
             if( result == '.') {
-                    var append = `<iframe src=' ${splitText[i]}'> </iframe>`;
->>>>>>> ebd2f7e541cedf0d66e6da9676eecb451635677f
+                    append = `<iframe src=' ${splitText[i]}'> </iframe>`;
                     console.log(append);
+                    
                     break;
                 };
             }
         console.log(textInput);
+        var ifr = document.getElementsByClassName('ifr');
+        ifr[ifr.length-1].innerHTML = append;
     }
