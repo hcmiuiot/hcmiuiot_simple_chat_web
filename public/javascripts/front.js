4c12ba128feb01a680ee14f1069148f9647a32a4
tra_h <tuonglam1510@hotmail.com>
2020-07-09 11:32:45 +0700

    frontjs to separate file

---
+++ public/javascripts/front.js
@@ -0,0 +1,117 @@
+//take username
+var username;
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#btnUsername').onclick = () => {
+        username = document.querySelector('#usernameInput').value;
+    };
+});
+
+
+var clientSocket = io();
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#send-container').onsubmit = function (
+        e
+    ) {
+        e.preventDefault();
+        clientSocket.emit(
+            'chat message',
+            username,
+            document.getElementById('txt-input').value
+        );
+        document.getElementById('txt-input').value = '';
+        console.log('send msg');
+    };
+});
+
+clientSocket.on('connect', () => {
+    console.log('Connected to server!');
+});
+
+clientSocket.on('chat message', (msgsname, msg) => {
+   
+    var node = document.createElement('SECTION');
+    var kirby = document.createElement("IMG");
+    kirby.setAttribute('class', 'ava');
+    kirby.setAttribute('src', 'https://www.spriters-resource.com/resources/sheet_icons/65/67841.png');
+
+    if (msgsname === username) {
+        setAtt(node, 'class', 'message -right');
+
+        setAtt(node, 'id', 'right-msg');
+        //Add msg:
+
+        var nodemsg = document.createElement('DIV');
+        var nodename = document.createElement("I");
+        nodemsg.setAttribute(
+            'class',
+            'nes-balloon from-right'
+        );
+        nodename.setAttribute(
+            'style',
+            'color: lightgrey; margin-bottom: 0px; font-size: 20px '
+        );
+        var msgnode = document.createTextNode(msg);
+        var namenode = document.createTextNode(msgsname);
+        nodemsg.appendChild(msgnode);
+        nodename.appendChild(namenode);
+        node.appendChild(nodemsg);
+        node.appendChild(nodename);
+        node.appendChild(kirby);
+        document.getElementById('msg-area').appendChild(node);
+        console.log(node);
+        darkmode();
+
+    }
+    else {
+        setAtt(node, 'class', 'message -left');
+        setAtt(node, 'id', 'left-msg');
+        //Add msg:
+        var nodemsg = document.createElement('P');
+
+        var nodename = document.createElement("I");
+        nodemsg.setAttribute(
+            'class',
+            'nes-balloon from-left'
+        );
+        nodename.setAttribute(
+            'style',
+            'font-style: oblique; font-size: 20; color: pink'
+        );
+        var msgnode = document.createTextNode(msg);
+        var namenode = document.createTextNode(msgsname);
+        nodename.appendChild(namenode);
+        nodemsg.appendChild(msgnode);
+        node.appendChild(nodename);
+        node.appendChild(nodemsg);
+        node.appendChild(kirby);
+        document.getElementById('msg-area').appendChild(node);
+        darkmode();
+
+    }
+    document.querySelector(
+        '#msg-area'
+    ).scrollTop = document.querySelector('#msg-area').scrollHeight;
+    // end here
+});
+
+
+function setAtt(node, att, attvalues) {
+    node.setAttribute(att, attvalues);
+};
+
+
+
+function darkmode() {
+
+    if ($("#darkbox").is(":checked")) {
+        $(".bg-image").attr("src", "/images/SnowNight.png");
+        $(".ava").attr('src', '/images/rsz_3341.png');
+        $(".nes-balloon").addClass("is-dark");
+    }
+    else {
+        $(".bg-image").attr("src", "/images/Snow.png");
+        $(".nes-balloon").removeClass("is-dark");
+        $(".ava").attr('src', 'https://www.spriters-resource.com/resources/sheet_icons/65/67841.png');
+    }
+}
