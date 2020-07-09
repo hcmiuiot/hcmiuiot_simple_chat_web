23c0de3c6e381ed21e2615cd302309cc91dad087
Datdeptrai0609 <Datdeptrai0609>
2020-07-09 15:59:05 +0700

    iframe fucntion

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -8,7 +8,7 @@
         for(let i=0; i<=splitText.length-1; i++) {
             var result =splitText[i].split('').find(char => char == '.');
             if( result == '.') {
-                    append = `<iframe src=' ${splitText[i]}'> </iframe>`;
+                    append = `<iframe width="560" height="315" src="${result}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
                     console.log(append);
                     
                     break;
